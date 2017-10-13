4eb0a39484aad35bb2d1887a52e592aff6838bff
783081406 <783081406ccj@gmail.com>
2017-10-13 11:49:16 +0800

    增加User JavaBean

---
+++ src/main/java/com/ccjjltx/domain/User.java
@@ -0,0 +1,73 @@
+package com.ccjjltx.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by ccjjltx on 2017/10/13.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@Entity
+@Table
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    private int uType;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getuType() {
+        return uType;
+    }
+
+    public void setuType(int uType) {
+        this.uType = uType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User that = (User) o;
+        if (id != that.id) return false;
+        if (uType != that.uType) return false;
+        if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + uType;
+        return result;
+    }
+}
