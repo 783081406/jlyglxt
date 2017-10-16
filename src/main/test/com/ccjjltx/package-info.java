d85bca0778ae98db72807ca64534262a9b5e29a6
783081406 <783081406ccj@gmail.com>
2017-10-16 23:21:00 +0800

    解释相关目录

---
+++ src/main/test/com/ccjjltx/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ccjjltx on 2017/10/16.
+ * 对应的java下的目录结构
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx;
---
+++ src/main/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by ccjjltx on 2017/10/16.
+ * 单元测试，测试相应java类的相关功能
+ *
+ * @author ccj
+ * @version 1.0
+ */
