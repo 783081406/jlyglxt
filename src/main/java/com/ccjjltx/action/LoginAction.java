d1728a325da6ab56482b0b251062cb132e4434a0
783081406 <783081406ccj@gmail.com>
2017-10-16 14:56:00 +0800

    验证用户名和密码Action相关Controller

--- src/main/java/com/ccjjltx/action/LoginAction.java
+++ src/main/java/com/ccjjltx/action/LoginAction.java
@@ -1,8 +1,15 @@
 package com.ccjjltx.action;
 
+import com.ccjjltx.dao.UserDAO;
+import com.ccjjltx.domain.User;
 import com.opensymphony.xwork2.ActionSupport;
+import net.sf.json.JSONObject;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ccjjltx on 2017/10/11.
@@ -18,6 +25,11 @@ public class LoginAction extends ActionSupport {
     private String userName;
     //登录的密码
     private String password;
+    //验证的结果信息如果错误将会返回给首页
+    private String result;
+    //得到UserDAO类
+    @Resource(name = "userDAO")
+    private UserDAO userDAO;
 
     public String getUserName() {
         return userName;
@@ -35,14 +47,41 @@ public class LoginAction extends ActionSupport {
         this.password = password;
     }
 
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
     /**
      * 验证用户登录
      *
      * @return 返回success或者error，分别打包成json和redict
      */
     public String CheckForntLogin() {
-        System.out.println(getUserName());
-        System.out.println(getPassword());
-        return SUCCESS;
+        if (getUserName() != null && getPassword() != null) {
+            User user = new User(getUserName(), getPassword());
+            int iResult = userDAO.checkUser(user);
+            switch (iResult) {
+                case 1:
+                    setError("用户名错误！");
+                    return ERROR;
+                case 2:
+                    setError("密码错误！");
+                    return ERROR;
+                default:
+                    return SUCCESS;
+            }
+        }
+        return "first";
+    }
+
+    public void setError(String message) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("message", message);
+        JSONObject json = JSONObject.fromObject(map);//将map对象转换成json类型数据
+        result = json.toString();//给result赋值，传递给页面
     }
 }
