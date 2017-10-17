cd362ba2f1a456f7cb143d70c453c297df8a90dd
783081406 <783081406ccj@gmail.com>
2017-10-17 23:20:24 +0800

    增加getAllUser和getUserNumber相关方法

--- src/main/java/com/ccjjltx/dao/UserDAO.java
+++ src/main/java/com/ccjjltx/dao/UserDAO.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/10/13.
@@ -43,5 +45,34 @@ public class UserDAO {
         return 3;
     }
 
+    /**
+     * 得到所有User数据
+     *
+     * @param offset 起始Number
+     * @param rows   行数
+     * @return 所有的User
+     */
+    public List<User> getAllUser(int offset, int rows) {
+        //得到Session
+        Session session = factory.getCurrentSession();
+        //总查询语句
+        String hql = "from User user";
+        Query query = session.createQuery(hql).setFirstResult(offset).setMaxResults(rows);
+        List<User> list = (List<User>) query.list();
+        return list;
+    }
+
+    /**
+     * 得到所有User的总条数
+     *
+     * @return 总条数
+     */
+    public int getAllUserNumber() {
+        //得到Session
+        Session session = factory.getCurrentSession();
+        //得到总条数
+        String hql = "select count(*) from User user";
+        return (int) session.createQuery(hql).uniqueResult();
+    }
 
 }
