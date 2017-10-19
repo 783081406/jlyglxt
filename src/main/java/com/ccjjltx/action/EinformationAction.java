1321db0810c89fcbcc768a8d09b9c39b3edb0ff7
783081406 <783081406ccj@gmail.com>
2017-10-19 22:53:10 +0800

    员工信息增删改查

---
+++ src/main/java/com/ccjjltx/action/EinformationAction.java
@@ -0,0 +1,17 @@
+package com.ccjjltx.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by ccjjltx on 2017/10/19.
+ * 员工信息增删改查
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@Controller
+@Scope("prototype")
+public class EinformationAction extends ActionSupport {
+}
