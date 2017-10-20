fa00d215293c930091ff3323a3e0a68debd51d1f
783081406 <783081406ccj@gmail.com>
2017-10-21 00:35:40 +0800

    封装getAllInformation，得到所有和特定的信息

--- src/main/java/com/ccjjltx/dao/EinformationDAO.java
+++ src/main/java/com/ccjjltx/dao/EinformationDAO.java
@@ -1,10 +1,15 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Einformation;
+import com.ccjjltx.domain.User;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/10/19.
@@ -18,5 +23,61 @@ import javax.annotation.Resource;
 public class EinformationDAO {
     @Resource(name = "sessionFactory")
     private SessionFactory factory;
+    @Resource(name = "userDAO")
+    private UserDAO userDAO;
+
+    /**
+     * 得到所有员工数据
+     *
+     * @param offset   起始的行数
+     * @param rows     显示的行数
+     * @param name     查询提交过来name数据
+     * @param userName 查询提交过来的userName数据
+     * @return 得到所有的数据返回List
+     */
+    public List<Einformation> getAllInformation(int offset, int rows, String name, String userName) {
+        Session session = factory.getCurrentSession();
+        //总查询语句
+        String hql = "from Einformation einformation";
+        Query query;
+        User db_user;
+        //搜索引擎有提交数据过来触发
+        if (name != null && userName != null) {
+            //表示同时有name和userName
+            db_user = userDAO.searchUser(userName);
+            if (db_user == null) {
+                //如果用户账户错误直接返回null
+                return null;
+            } else {
+                //搜索框的两个值都有开启精确查询查询
+                hql += " where einformation.name=:name and einformation.user=:user";
+                query = session.createQuery(hql).setParameter("name", name).setParameter("user", db_user);
+            }
+        } else {
+            //进入该判断表示搜索框只填入一个或者什么都没填
+            if (name != null) {
+                //搜索框的name值都开启了模糊查询
+                hql += " where name like :name";
+                query = session.createQuery(hql).setParameter("name", name);
+            } else if (userName != null) {
+                //搜索框的userName值不为0，开启精确查询
+                db_user = userDAO.searchUser(userName);
+                if (db_user == null) {
+                    //如果用户账户错误直接返回null
+                    return null;
+                } else {
+                    //查询不为空开启精确查询
+                    hql += " where einformation.user=:user";
+                    query = session.createQuery(hql).setParameter("user", db_user);
+                }
+            } else {
+                //搜索框什么都没有输入
+                query = session.createQuery(hql);
+            }
+        }
+        query = query.setFirstResult(offset).setMaxResults(rows);
+        return (List<Einformation>) query.list();
+    }
+
 
 }
