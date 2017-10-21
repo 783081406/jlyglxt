bcf27be41f07547516593932e46abe6c54958f16
783081406 <783081406ccj@gmail.com>
2017-10-21 15:20:50 +0800

    工具类：格式化时间转String

---
+++ src/main/java/com/ccjjltx/utils/MyDateFormat.java
@@ -0,0 +1,28 @@
+package com.ccjjltx.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ccjjltx on 2017/10/21.
+ * 工具类：用于格式化从数据库得到的Date，格式化成String
+ * 不可实例化
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class MyDateFormat {
+    //私有化不可实例
+    private MyDateFormat() {
+    }
+
+    /**
+     * 装饰模式
+     *
+     * @param date 传入date值
+     * @return 字符串类型
+     */
+    public static String format(Date date) {
+        return new SimpleDateFormat("yyyy-MM-dd").format(date);
+    }
+}
