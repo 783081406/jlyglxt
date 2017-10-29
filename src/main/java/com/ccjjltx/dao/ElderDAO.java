61c41dfcdc0825667d3714ccd440c8753f3dd02a
783081406 <783081406ccj@gmail.com>
2017-10-29 20:52:12 +0800

    新增getSearchElder方法根据ename得到Elder实例化

--- src/main/java/com/ccjjltx/dao/ElderDAO.java
+++ src/main/java/com/ccjjltx/dao/ElderDAO.java
@@ -1,5 +1,8 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Elder;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,5 +22,16 @@ public class ElderDAO {
     @Resource(name = "sessionFactory")
     private SessionFactory factory;
 
-
+    /**
+     * 根据ename得到Elder实例化
+     *
+     * @param ename 老人省略表中的用户名
+     * @return Elder的实例化或null
+     */
+    public Elder getSearchElder(String ename) {
+        Session session = factory.getCurrentSession();
+        String hql = "from Elder elder where ename=:ename";
+        Query query = session.createQuery(hql).setParameter("ename", ename);
+        return (Elder) query.uniqueResult();
+    }
 }
