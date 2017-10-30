894d8dfdbf598cc0bf9fe740cb4e225723930852
783081406 <783081406ccj@gmail.com>
2017-10-30 13:52:53 +0800

    添加getIdElderName方法，用于得所有的Elder信息

---
+++ src/main/java/com/ccjjltx/action/ElderAction.java
@@ -0,0 +1,61 @@
+package com.ccjjltx.action;
+
+import com.ccjjltx.dao.ElderDAO;
+import com.ccjjltx.domain.Elder;
+import com.opensymphony.xwork2.ActionSupport;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by ccjjltx on 2017/10/30.
+ * 下拉框选择老人名字的时候使用
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@Controller
+@Scope("prototype")
+public class ElderAction extends ActionSupport {
+    private JSONArray result;
+    @Resource(name = "elderDAO")
+    private ElderDAO elderDAO;
+
+    public JSONArray getResult() {
+        return result;
+    }
+
+    public void setResult(JSONArray result) {
+        this.result = result;
+    }
+
+    public ElderDAO getElderDAO() {
+        return elderDAO;
+    }
+
+    public void setElderDAO(ElderDAO elderDAO) {
+        this.elderDAO = elderDAO;
+    }
+
+    /**
+     * 下拉框选择用户帐户使用
+     * 场景：信息中心->房间信息
+     *
+     * @return 设置为SUCCESS
+     */
+    public String getIdElderName() {
+        result = new JSONArray();
+        List<Elder> list = elderDAO.getAllElder();
+        for (Elder elder : list) {
+            JSONObject js = new JSONObject();
+            js.put("eId", elder.getEId());
+            js.put("ename", elder.getEname());
+            result.add(js);
+        }
+        return SUCCESS;
+    }
+}
