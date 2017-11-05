57bf09ce06007939bb3f0339ca212fc6ce9e379c
783081406 <783081406ccj@gmail.com>
2017-11-05 20:46:33 +0800

    增加getAllInformation方法

--- src/main/java/com/ccjjltx/dao/FamilyinformationDAO.java
+++ src/main/java/com/ccjjltx/dao/FamilyinformationDAO.java
@@ -1,10 +1,15 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Elderlyinformation;
+import com.ccjjltx.domain.Familyinformation;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/11/5.
@@ -18,4 +23,14 @@ import javax.annotation.Resource;
 public class FamilyinformationDAO {
     @Resource(name = "sessionFactory")
     private SessionFactory factory;
+    @Resource(name = "elderlyinformationDAO")
+    private ElderlyinformationDAO elderlyinformationDAO;
+
+    public List<Familyinformation> getAllInformation(int eiId) {
+        Session session = factory.getCurrentSession();
+        String hql = "from Familyinformation familyinformation where elderlyinformation=:elderlyinformation";
+        Elderlyinformation elderlyinformation = elderlyinformationDAO.getSearchInformation(eiId);
+        Query query = session.createQuery(hql).setParameter("elderlyinformation", elderlyinformation);
+        return (List<Familyinformation>) query.list();
+    }
 }
