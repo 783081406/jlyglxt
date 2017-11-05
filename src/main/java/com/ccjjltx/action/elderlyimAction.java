7ed9d89773cb39ac6d833f654f2dee04fe4a9865
783081406 <783081406ccj@gmail.com>
2017-11-05 14:52:15 +0800

    创建信息查看的Controller

---
+++ src/main/java/com/ccjjltx/action/elderlyimAction.java
@@ -0,0 +1,18 @@
+package com.ccjjltx.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by ccjjltx on 2017/11/5.
+ * 对信息查看的增删除查改
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@Controller
+@Scope("prototype")
+public class elderlyimAction extends ActionSupport {
+
+}
