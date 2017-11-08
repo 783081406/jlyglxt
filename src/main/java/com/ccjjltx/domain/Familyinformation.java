7a7e3c0ace70884c3f50302b3cffc076176e904d
783081406 <783081406ccj@gmail.com>
2017-11-08 19:09:01 +0800

    增加@Table注解

--- src/main/java/com/ccjjltx/domain/Familyinformation.java
+++ src/main/java/com/ccjjltx/domain/Familyinformation.java
@@ -11,6 +11,7 @@ import java.io.Serializable;
  * @version 1.0
  */
 @Entity
+@Table
 public class Familyinformation implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
