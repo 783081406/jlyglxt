c4668e1726522c9d2cbbda96a2b82d778b032c9f
783081406 <783081406ccj@gmail.com>
2017-11-08 20:34:23 +0800

    测试CasehistoryDAO相关的功能

---
+++ src/main/test/com/ccjjltx/dao/CasehistoryDAOTest.java
@@ -0,0 +1,21 @@
+package com.ccjjltx.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by ccjjltx on 2017/11/8.
+ * 测试CasehistoryDAO相关的功能
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:beans.xml")
+public class CasehistoryDAOTest {
+    @Resource(name = "casehistoryDAO")
+    private CasehistoryDAO casehistoryDAO;
+}
