e1acef80274a8ea5b19cce38d34dd0d876e25f6f
783081406 <783081406ccj@gmail.com>
2017-11-13 13:59:56 +0800

    测试getAllInformation方法

--- src/main/test/com/ccjjltx/dao/CasehistoryDAOTest.java
+++ src/main/test/com/ccjjltx/dao/CasehistoryDAOTest.java
@@ -1,10 +1,15 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Casehistory;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/11/8.
@@ -18,4 +23,37 @@ import javax.annotation.Resource;
 public class CasehistoryDAOTest {
     @Resource(name = "casehistoryDAO")
     private CasehistoryDAO casehistoryDAO;
+
+    /**
+     * 验证：ename为null的时候是否返回总条数
+     */
+    @Test
+    @Transactional
+    public void testGetAllInformation1() {
+        //当时数据库只有十五条数据
+        List<Casehistory> list = casehistoryDAO.getAllInformation(0, 20, null);
+        Assert.assertEquals(15, list.size());
+    }
+
+    /**
+     * 验证：输入正确的ename时候是否返回准确值
+     */
+    @Test
+    @Transactional
+    public void testGetAllInformation2() {
+        List<Casehistory> list = casehistoryDAO.getAllInformation(0, 10, "张志新");
+        Assert.assertEquals(1, list.size());
+    }
+
+    /**
+     * 验证：假ename时候，是否返回null
+     */
+    @Test
+    @Transactional
+    public void testGetAllInformation3() {
+        List<Casehistory> list = casehistoryDAO.getAllInformation(0, 10, "ccj");
+        Assert.assertNull(list);
+    }
+
+
 }
