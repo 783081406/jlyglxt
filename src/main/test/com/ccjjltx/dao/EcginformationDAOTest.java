de866982d910099de0b2711875e5fd6736954d96
783081406 <783081406ccj@gmail.com>
2017-11-15 14:19:00 +0800

    测试新增的addInformation方法

--- src/main/test/com/ccjjltx/dao/EcginformationDAOTest.java
+++ src/main/test/com/ccjjltx/dao/EcginformationDAOTest.java
@@ -1,14 +1,18 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Casehistory;
 import com.ccjjltx.domain.Ecginformation;
+import com.ccjjltx.utils.MyDateFormat;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.text.ParseException;
 import java.util.List;
 
 /**
@@ -23,6 +27,8 @@ import java.util.List;
 public class EcginformationDAOTest {
     @Resource(name = "ecginformationDAO")
     private EcginformationDAO ecginformationDAO;
+    @Resource(name = "casehistoryDAO")
+    private CasehistoryDAO casehistoryDAO;
 
     /**
      * 验证：是否能根据主键得到所有的心电信息
@@ -36,5 +42,27 @@ public class EcginformationDAOTest {
         Assert.assertEquals(2, list.size());
     }
 
+    /**
+     * 验证：是否能新增信息
+     */
+    @Test
+    @Transactional
+    @Rollback
+    public void testAddInformation() {
+        try {
+            //得到chId为1的心电信息，得到其中大小
+            int result1 = ecginformationDAO.getAllInformation(1).size();
+            Ecginformation eb = new Ecginformation("1", "1", "1", "1", "1", MyDateFormat.parse("2017-11-15"));
+            //得到一个casehistory实例化
+            Casehistory casehistory = casehistoryDAO.getSearchInformation(1);
+            eb.setCasehistory(casehistory);
+            //增加操作
+            ecginformationDAO.addInformation(eb);
+            int result2 = ecginformationDAO.getAllInformation(1).size();
+            Assert.assertEquals(result1 + 1, result2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
