182ca29d33bfcca7e24c1e6d67a695461f1c35db
783081406 <783081406ccj@gmail.com>
2017-11-15 15:43:49 +0800

    测试getAllInformation方法

--- src/main/test/com/ccjjltx/dao/MedicalrecordDAOTest.java
+++ src/main/test/com/ccjjltx/dao/MedicalrecordDAOTest.java
@@ -1,8 +1,11 @@
 package com.ccjjltx.dao;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -18,4 +21,17 @@ import javax.annotation.Resource;
 public class MedicalrecordDAOTest {
     @Resource(name = "medicalrecordDAO")
     private MedicalrecordDAO medicalrecordDAO;
+    @Resource(name = "casehistoryDAO")
+    private CasehistoryDAO casehistoryDAO;
+
+    /**
+     * 验证：是否能根据chId得到全部的就医记录实例化
+     */
+    @Test
+    @Transactional
+    public void getAllInformation() {
+        int result = medicalrecordDAO.getAllInformation(1).size();
+        //测试的时候数据库chId为1的就医记录数据为2
+        Assert.assertEquals(2, result);
+    }
 }
