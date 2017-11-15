6babee53396689c83c086956b3aff8efc18f95a1
783081406 <783081406ccj@gmail.com>
2017-11-15 13:48:30 +0800

    新增getAllInformation根据chId得到全部的心电信息

--- src/main/java/com/ccjjltx/dao/EcginformationDAO.java
+++ src/main/java/com/ccjjltx/dao/EcginformationDAO.java
@@ -1,10 +1,15 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Casehistory;
+import com.ccjjltx.domain.Ecginformation;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/11/8.
@@ -18,5 +23,21 @@ import javax.annotation.Resource;
 public class EcginformationDAO {
     @Resource(name = "sessionFactory")
     private SessionFactory factory;
+    @Resource(name = "casehistoryDAO")
+    private CasehistoryDAO casehistoryDAO;
 
+    /**
+     * 根据casehistory病历的主键得到相关的心电信息实例化
+     *
+     * @param chId casehistory主键
+     * @return list集合
+     */
+    public List<Ecginformation> getAllInformation(int chId) {
+        Session session = factory.getCurrentSession();
+        //得到casehistory实例化
+        Casehistory db_casehistory = casehistoryDAO.getSearchInformation(chId);
+        String hql = "from Ecginformation ecginformation where casehistory=:casehistory";
+       Query query = session.createQuery(hql).setParameter("casehistory", db_casehistory);
+        return (List<Ecginformation>) query.list();
+    }
 }
