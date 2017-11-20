47274b1bd227cac582a3a4078b968eb67ead7d05
783081406 <783081406ccj@gmail.com>
2017-11-20 15:23:27 +0800

    新增加getAllInformation，得到所有的数据

--- src/main/java/com/ccjjltx/dao/BillboardsDAO.java
+++ src/main/java/com/ccjjltx/dao/BillboardsDAO.java
@@ -1,10 +1,14 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Billboards;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/11/20.
@@ -18,4 +22,19 @@ import javax.annotation.Resource;
 public class BillboardsDAO {
     @Resource(name = "sessionFactory")
     private SessionFactory factory;
+
+    /**
+     * 得到全部的信息
+     *
+     * @param offset 起始行数
+     * @param rows   返回行数
+     * @return List集合数据
+     */
+    public List<Billboards> getAllInformation(int offset, int rows) {
+        Session session = factory.getCurrentSession();
+        String hql = "from Billboards billboards order by bid desc";
+        Query query = session.createQuery(hql).setFirstResult(offset).setMaxResults(rows);
+        return (List<Billboards>) query.list();
+    }
+
 }
