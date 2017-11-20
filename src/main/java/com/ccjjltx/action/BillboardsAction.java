6008ee8b120c1acd7649568e0bc6ec0021bb5d34
783081406 <783081406ccj@gmail.com>
2017-11-20 16:47:08 +0800

    新增getAllInformation，得到所有的数据

--- src/main/java/com/ccjjltx/action/BillboardsAction.java
+++ src/main/java/com/ccjjltx/action/BillboardsAction.java
@@ -1,12 +1,16 @@
 package com.ccjjltx.action;
 
 import com.ccjjltx.dao.BillboardsDAO;
+import com.ccjjltx.domain.Billboards;
+import com.ccjjltx.utils.ResourcePath;
 import com.opensymphony.xwork2.ActionSupport;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/11/20.
@@ -29,8 +33,12 @@ public class BillboardsAction extends ActionSupport {
     private String btitle;
     private String bcontent;
     private Byte isSelect;
+    ////////////////////上传//////////////////////////////////
 
-    //////////////////////////////////////////////////////
+    // 直接在struts.xml文件中配置的属性
+    private String savePath;
+
+    /////////////////////////////////////////////////////
     public BillboardsDAO getBillboardsDAO() {
         return billboardsDAO;
     }
@@ -102,4 +110,44 @@ public class BillboardsAction extends ActionSupport {
     public void setIsSelect(Byte isSelect) {
         this.isSelect = isSelect;
     }
+
+    public String getSavePath() {
+        return savePath;
+    }
+
+    public void setSavePath(String savePath) {
+        this.savePath = savePath;
+    }
+
+    /**
+     * 得到总数据
+     *
+     * @return 返回Json数据
+     */
+    public String getAllInformation() {
+        //得到起始的行数
+        int offset = (getPage() - 1) * getRows();
+        //得到所有数据
+        List<Billboards> list = billboardsDAO.getAllInformation(offset, getRows());
+        //得到总条数
+        int total = billboardsDAO.getAllInformationNumber();
+        result = new JSONObject();
+        result.put("total", total);
+        JSONArray jsonArray = new JSONArray();
+        String tempPath = "../../../reception/img/billboards";
+        for (Billboards bb : list) {
+            JSONObject js = new JSONObject();
+            js.put("bid", bb.getBid());
+            js.put("bpath", tempPath + "/" + bb.getBpath());
+            js.put("btitle", bb.getBtitle());
+            js.put("bcontent", bb.getBcontent());
+            js.put("bcontent", bb.getBcontent());
+            js.put("checked", bb.getIsSelect() == 0x00 ? false : true);
+            jsonArray.add(js);
+        }
+        result.put("rows", jsonArray);
+        return SUCCESS;
+    }
+
+
 }
