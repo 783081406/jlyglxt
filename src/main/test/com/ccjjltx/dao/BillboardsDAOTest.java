204ca9e084503df86f872ebb1dfcc657dbf3aae2
783081406 <783081406ccj@gmail.com>
2017-11-20 15:03:05 +0800

    初始化

---
+++ src/main/test/com/ccjjltx/dao/BillboardsDAOTest.java
@@ -0,0 +1,21 @@
+package com.ccjjltx.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by ccjjltx on 2017/11/20.
+ * 测试BillboardsDAO相关的功能
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:beans.xml")
+public class BillboardsDAOTest {
+    @Resource(name = "billboardsDAO")
+    private BillboardsDAO billboardsDAO;
+}
