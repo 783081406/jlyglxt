d325b3f6e153c9cf2c312f72600cb980f7820932
783081406 <783081406ccj@gmail.com>
2017-11-20 23:35:33 +0800

    新增加deleteFile删除文件

--- src/main/java/com/ccjjltx/utils/MyFile.java
+++ src/main/java/com/ccjjltx/utils/MyFile.java
@@ -45,4 +45,24 @@ public class MyFile {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 删除单个文件
+     *
+     * @param fileName 要删除的文件的文件名
+     * @return 单个文件删除成功返回true，否则返回false
+     */
+    public static boolean deleteFile(String fileName) {
+        File file = new File(fileName);
+        // 如果文件路径所对应的文件存在，并且是一个文件，则直接删除
+        if (file.exists() && file.isFile()) {
+            if (file.delete()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
