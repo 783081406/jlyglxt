52db999eef44e550a6933cb92ae01156394b1a03
783081406 <783081406ccj@gmail.com>
2017-11-20 16:12:26 +0800

    测试ResourcePathTest的parse方法

---
+++ src/main/test/com/ccjjltx/utils/ResourcePathTest.java
@@ -0,0 +1,20 @@
+package com.ccjjltx.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ccjjltx on 2017/11/20.
+ * 测试ResourcePath类
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class ResourcePathTest {
+    @Test
+    public void testParse() {
+        String str = "e:\\\\123\\\\1234";
+        String result = ResourcePath.parse(str);
+        Assert.assertEquals("e:\\123\\1234", result);
+    }
+}
