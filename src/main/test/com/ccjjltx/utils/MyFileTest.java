9c38dca5c71c3035325d9acd00de3cdde63eb9d5
783081406 <783081406ccj@gmail.com>
2017-11-20 23:23:36 +0800

    测试copyFile，是否具有复制文件功能

---
+++ src/main/test/com/ccjjltx/utils/MyFileTest.java
@@ -0,0 +1,20 @@
+package com.ccjjltx.utils;
+
+import org.junit.Test;
+
+/**
+ * Created by ccjjltx on 2017/11/20.
+ * MyFile工具类
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class MyFileTest {
+    /**
+     * 验证：是否具有复制文件功能
+     */
+    @Test
+    public void testCopyFile() {
+        MyFile.copyFile("E:\\123\\4.jpg", "E:\\123\\456\\4.jpg");
+    }
+}
