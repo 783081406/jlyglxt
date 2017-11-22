cd5556ad7cfa93f56645cfb18afa2f206bcee975
783081406 <783081406ccj@gmail.com>
2017-11-22 16:53:21 +0800

    增加getAllInformation，得到所有数据信息

---
+++ src/main/java/com/ccjjltx/dao/ServiceitemsDAO.java
@@ -0,0 +1,43 @@
+package com.ccjjltx.dao;
+
+import com.ccjjltx.domain.Serviceitems;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by ccjjltx on 2017/11/22.
+ * Serviceitems服务项目相关增删查改操作
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@Service
+@Transactional
+public class ServiceitemsDAO {
+    @Resource(name = "sessionFactory")
+    private SessionFactory factory;
+
+    /**
+     * 得到全部的信息
+     *
+     * @param offset 起始行数
+     * @param rows   返回行数
+     * @return List集合数据
+     */
+    public List<Serviceitems> getAllInformation(int offset, int rows) {
+        Session session = factory.getCurrentSession();
+        String hql = "from Serviceitems serviceitems order by sid desc";
+        Query query = session.createQuery(hql).setFirstResult(offset).setMaxResults(rows);
+        return (List<Serviceitems>) query.list();
+    }
+
+
+
+
+}
