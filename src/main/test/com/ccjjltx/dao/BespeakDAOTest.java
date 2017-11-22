9a7c178fcef0be241a8b12163843f56c2117b21b
783081406 <783081406ccj@gmail.com>
2017-11-22 23:32:15 +0800

    测试getAllUnhandleInformation方法

--- src/main/test/com/ccjjltx/dao/BespeakDAOTest.java
+++ src/main/test/com/ccjjltx/dao/BespeakDAOTest.java
@@ -43,4 +43,15 @@ public class BespeakDAOTest {
         int result2 = bespeakDAO.getAllHandleInformationNumber();
         Assert.assertEquals(result, result2);
     }
+
+    /**
+     * 验证：是否能返回所有已经处理的数据
+     */
+    @Test
+    @Transactional
+    public void testGetAllUnhandleInformation() {
+        //测试该方法时，数据库的数据已处理为12
+        int result = bespeakDAO.getAllUnhandleInformation(0, 20).size();
+        Assert.assertEquals(12, result);
+    }
 }
