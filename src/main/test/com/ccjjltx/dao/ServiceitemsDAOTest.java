e7c1b991818c18f7368d277a71e8970480eef5ac
783081406 <783081406ccj@gmail.com>
2017-11-22 17:25:09 +0800

    测试reelectInformation方法

--- src/main/test/com/ccjjltx/dao/ServiceitemsDAOTest.java
+++ src/main/test/com/ccjjltx/dao/ServiceitemsDAOTest.java
@@ -1,13 +1,16 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Serviceitems;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/11/22.
@@ -44,6 +47,30 @@ public class ServiceitemsDAOTest {
         Assert.assertEquals(12, result);
     }
 
+    /**
+     * 验证：是否所有的值都改为0
+     */
+    @Test
+    @Transactional
+    @Rollback
+    public void testReelectInformation() {
+        //得到得到的第一条数据
+        //测试时，该条数据的isSelect是1;
+        List<Serviceitems> list = serviceitemsDAO.getAllInformation(0, 1);
+        int sisSelect = 2;
+        for (Serviceitems si : list) {
+            sisSelect = si.getIsSelect();
+        }
+        //执行重选
+        serviceitemsDAO.reelectInformation();
+        //再次得到数据，查看是否为0
+        List<Serviceitems> list2 = serviceitemsDAO.getAllInformation(0, 1);
+        int sisSelect2 = 2;
+        for (Serviceitems si : list2) {
+            sisSelect2 = si.getIsSelect();
+        }
+        Assert.assertEquals(sisSelect - 1, sisSelect2);
+    }
 
 
 }
