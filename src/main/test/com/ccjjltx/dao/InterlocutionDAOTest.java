4ff6976209dd812eca1d6dfc96e7940ecc7ea139
783081406 <783081406ccj@gmail.com>
2017-11-23 16:20:52 +0800

    模板化

---
+++ src/main/test/com/ccjjltx/dao/InterlocutionDAOTest.java
@@ -0,0 +1,22 @@
+package com.ccjjltx.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by ccjjltx on 2017/11/23.
+ * 测试InterlocutionDAO相关功能
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:beans.xml")
+public class InterlocutionDAOTest {
+    @Resource(name = "interlocutionDAO")
+    private InterlocutionDAO interlocutionDAO;
+
+}
