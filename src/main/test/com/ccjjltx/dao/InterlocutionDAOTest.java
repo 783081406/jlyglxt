ee2c47168bb069e9fe0e777fbb1bbf36f039c253
783081406 <783081406ccj@gmail.com>
2017-11-23 16:37:41 +0800

    测试getAllInformation方法

--- src/main/test/com/ccjjltx/dao/InterlocutionDAOTest.java
+++ src/main/test/com/ccjjltx/dao/InterlocutionDAOTest.java
@@ -1,8 +1,11 @@
 package com.ccjjltx.dao;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -18,5 +21,16 @@ import javax.annotation.Resource;
 public class InterlocutionDAOTest {
     @Resource(name = "interlocutionDAO")
     private InterlocutionDAO interlocutionDAO;
+
+    /**
+     * 验证：是否得到全部的数据
+     */
+    @Test
+    @Transactional
+    public void testGetAllInformation() {
+        //测试该方法时数据库的数据为16
+        int result = interlocutionDAO.getAllInformation(0, 20).size();
+        Assert.assertEquals(16, result);
+    }
 
 }
