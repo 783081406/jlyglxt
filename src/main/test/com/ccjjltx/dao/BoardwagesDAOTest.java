f0cba45607ab06724e62d24ad6305ad674a33b50
783081406 <783081406ccj@gmail.com>
2017-11-24 23:26:01 +0800

    测试getSearchInformation方法

--- src/main/test/com/ccjjltx/dao/BoardwagesDAOTest.java
+++ src/main/test/com/ccjjltx/dao/BoardwagesDAOTest.java
@@ -1,5 +1,6 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Boardwages;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +32,16 @@ public class BoardwagesDAOTest {
         //测试该方法时，数据库的数据为1条
         int result = boardwagesDAO.getAllInformation().size();
         Assert.assertEquals(1, result);
+    }
+
+    /**
+     * 验证：是否能根据主键得到实例化
+     */
+    @Test
+    @Transactional
+    public void testGetSearchInformation() {
+        Boardwages bw = boardwagesDAO.getSearchInformation(1);
+        Assert.assertEquals(1, bw.getBwid());
     }
 
 }
