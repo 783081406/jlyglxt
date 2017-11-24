d902aac19f6b6de5021590af023094b14d53c4c0
783081406 <783081406ccj@gmail.com>
2017-11-25 00:12:21 +0800

    模板化

---
+++ src/main/java/com/ccjjltx/dao/NursingfeeDAO.java
@@ -0,0 +1,23 @@
+package com.ccjjltx.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by ccjjltx on 2017/11/25.
+ * 护理费相关操作
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@Service
+@Transactional
+public class NursingfeeDAO {
+    @Resource(name = "sessionFactory")
+    private SessionFactory factory;
+
+
+}
