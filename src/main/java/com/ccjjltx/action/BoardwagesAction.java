a9e8eb89f1dc20d50d4ac3fd28554694857c3689
783081406 <783081406ccj@gmail.com>
2017-11-24 23:47:38 +0800

    新增getAllInformation得到所有的数据

--- src/main/java/com/ccjjltx/action/BoardwagesAction.java
+++ src/main/java/com/ccjjltx/action/BoardwagesAction.java
@@ -1,11 +1,14 @@
 package com.ccjjltx.action;
 
 import com.ccjjltx.dao.BoardwagesDAO;
+import com.ccjjltx.domain.Boardwages;
+import com.opensymphony.xwork2.ActionSupport;
 import net.sf.json.JSONObject;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/11/24.
@@ -16,13 +19,14 @@ import javax.annotation.Resource;
  */
 @Controller
 @Scope("prototype")
-public class BoardwagesAction {
+public class BoardwagesAction extends ActionSupport {
     @Resource(name = "boardwagesDAO")
     private BoardwagesDAO boardwagesDAO;
     private JSONObject result;
     ///////////////提交过来的数据////////////
     int highest;
     int minimum;
+
     ////////////////////////////////////////
     public BoardwagesDAO getBoardwagesDAO() {
         return boardwagesDAO;
@@ -54,5 +58,21 @@ public class BoardwagesAction {
 
     public void setMinimum(int minimum) {
         this.minimum = minimum;
+    }
+
+    /**
+     * 得到所有的信息
+     *
+     * @return Json数据
+     */
+    public String getAllInformation() {
+        result = new JSONObject();
+        //得到所有信息，目前共一条
+        List<Boardwages> list = boardwagesDAO.getAllInformation();
+        //得到第一条数据
+        Boardwages bw1 = list.get(0);
+        result.put("minimum", bw1.getMinimum());
+        result.put("highest", bw1.getHighest());
+        return SUCCESS;
     }
 }
