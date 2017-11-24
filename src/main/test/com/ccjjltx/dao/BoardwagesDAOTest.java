3bfc46e2277d00801128a592b38baa71763f6aae
783081406 <783081406ccj@gmail.com>
2017-11-24 23:24:02 +0800

    模板化

---
+++ src/main/test/com/ccjjltx/dao/BoardwagesDAOTest.java
@@ -0,0 +1,24 @@
+package com.ccjjltx.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by ccjjltx on 2017/11/24.
+ * 测试BoardwagesDAO相关方法
+ *
+ * @author ccj
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:beans.xml")
+public class BoardwagesDAOTest {
+    @Resource(name = "boardwagesDAO")
+    private BoardwagesDAO boardwagesDAO;
+}
