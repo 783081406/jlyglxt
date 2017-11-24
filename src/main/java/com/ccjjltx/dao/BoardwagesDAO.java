7b4c091a832bb39226977d53bc02399ad7988fb1
783081406 <783081406ccj@gmail.com>
2017-11-24 23:18:12 +0800

    新增getAllInformation得到所有的数据

--- src/main/java/com/ccjjltx/dao/BoardwagesDAO.java
+++ src/main/java/com/ccjjltx/dao/BoardwagesDAO.java
@@ -1,10 +1,14 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Boardwages;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2017/11/24.
@@ -15,4 +19,18 @@ import javax.annotation.Resource;
 public class BoardwagesDAO {
     @Resource(name = "sessionFactory")
     private SessionFactory factory;
+
+    /**
+     * 获得所有的数据
+     *
+     * @return list集合
+     */
+    public List<Boardwages> getAllInformation() {
+        Session session = factory.getCurrentSession();
+        String hql = "from Boardwages boardwages";
+        Query query = session.createQuery(hql);
+        return (List<Boardwages>) query.list();
+    }
+
+
 }
