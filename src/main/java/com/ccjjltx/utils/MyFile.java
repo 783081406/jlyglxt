af0e2772ef15f799deae678312a417b427371f9c
783081406 <783081406ccj@gmail.com>
2017-11-30 21:19:37 +0800

    修改部分注解与代码

--- src/main/java/com/ccjjltx/utils/MyFile.java
+++ src/main/java/com/ccjjltx/utils/MyFile.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 
 /**
  * Created by ccjjltx on 2017/11/20.
- * 文件复制
+ * 文件复制与删除
  *
  * @author ccj
  * @version 1.0
@@ -33,7 +33,6 @@ public class MyFile {
                 InputStream inStream = new FileInputStream(oldPath); //读入原文件
                 FileOutputStream fs = new FileOutputStream(newPath);
                 byte[] buffer = new byte[1024];
-                int length;
                 while ((byteread = inStream.read(buffer)) != -1) {
                     bytesum += byteread; //字节数 文件大小
                     fs.write(buffer, 0, byteread);
