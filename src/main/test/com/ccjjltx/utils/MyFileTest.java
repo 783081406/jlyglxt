908fab4684ca8c7d6d8ad9ffd15b06aec5698e6f
783081406 <783081406ccj@gmail.com>
2018-03-08 15:27:50 +0800

    弥补计算规则出错

--- src/main/test/com/ccjjltx/utils/MyFileTest.java
+++ src/main/test/com/ccjjltx/utils/MyFileTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by ccjjltx on 2017/11/20.
@@ -17,13 +18,16 @@ public class MyFileTest {
      * 验证：是否具有复制文件功能
      */
     @Test
-    public void testCopyFile() {
-        File file = new File("E:\\123\\456\\4.jpg");
-        Assert.assertFalse(file.exists());//断言不存在
-        MyFile.copyFile("E:\\123\\4.jpg", "E:\\123\\456\\4.jpg");//执行复制
-        Assert.assertTrue(file.exists());//复制之后存在
-        //为了方便下次测试，删除456下的图片
-        MyFile.deleteFile("E:\\123\\456\\4.jpg");
+    public void testCopyFile1() {
+        try {
+            MyFile myFile = new MyFile();//弥补计算规则出错
+            File file = new File("E:\\123\\456\\4.jpg");
+            Assert.assertFalse(file.exists());//断言不存在
+            MyFile.copyFile("E:\\123\\4.jpg", "E:\\123\\456\\4.jpg");//执行复制
+            Assert.assertTrue(file.exists());//复制之后存在
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -31,10 +35,14 @@ public class MyFileTest {
      */
     @Test
     public void testDeleteFile() {
-        MyFile.copyFile("E:\\123\\789\\5backup,jpg", "E:\\123\\789\\5.jpg");//执行复制
-        MyFile.deleteFile("E:\\123\\789\\5.jpg");
-        File file = new File("E:\\123\\789\\5.jpg");
-        Assert.assertFalse(file.exists());//断言不存在
+        try {
+            MyFile.copyFile("E:\\123\\789\\5backup,jpg", "E:\\123\\789\\5.jpg");//执行复制
+            MyFile.deleteFile("E:\\123\\789\\5.jpg");
+            File file = new File("E:\\123\\789\\5.jpg");
+            Assert.assertFalse(file.exists());//断言不存在
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
