646718fb59975f5b1dc20a1bd3fe6f0f51ab488f
783081406 <783081406ccj@gmail.com>
2018-03-08 15:06:03 +0800

    删除实例化

--- src/main/java/com/ccjjltx/utils/MyDateFormat.java
+++ src/main/java/com/ccjjltx/utils/MyDateFormat.java
@@ -7,15 +7,11 @@ import java.util.Date;
 /**
  * Created by ccjjltx on 2017/10/21.
  * 工具类：用于格式化从数据库得到的Date，格式化成String
- * 不可实例化
  *
  * @author ccj
  * @version 1.0
  */
 public class MyDateFormat {
-    //私有化不可实例
-    private MyDateFormat() {
-    }
 
     /**
      * 装饰模式
