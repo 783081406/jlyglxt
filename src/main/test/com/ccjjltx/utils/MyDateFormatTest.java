5ab66f9ca6c9a15336c7bea64e9e21b0d6e11a3a
783081406 <783081406ccj@gmail.com>
2018-03-08 14:19:03 +0800

    修改测试数据

---
+++ src/main/test/com/ccjjltx/utils/MyDateFormatTest.java
@@ -0,0 +1,40 @@
+package com.ccjjltx.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ccjjltx on 2018/3/8.
+ * 对MyDateFormat进行单元测试
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class MyDateFormatTest {
+
+    /**
+     * 验证：是否能转换
+     */
+    @Test
+    public void testFormat() {
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        Assert.assertEquals(date, MyDateFormat.format(new Date()));
+    }
+
+    /**
+     * 验证：是否能转换
+     */
+    @Test
+    public void testParse() {
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd").parse("2018-01-01");
+            Assert.assertEquals(date, MyDateFormat.parse("2018-01-01"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
